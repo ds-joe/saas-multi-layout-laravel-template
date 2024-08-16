eed93b871356384ca42b15827fc87018d0790c2a
Joe <youssefbibawy@gmail.com>
2024-08-16 11:16:25 -0500

    Add database models and migrations

---
+++ app/Models/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'price',
+        'status',
+        'credits',
+        'session_id',
+        'user_id',
+        'package_id'
+    ];
+
+    /**
+     * Who owner of this transaction.
+     * 
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
+use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('1234567')
+        ]);
+
+        Feature::create([
+            'image' => 'https://static-00.iconduck/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate Sum',
+            'description' => 'Calculate sum of two numbers',
+            'required_credits' => 1,
+            'active' => true
+        ]);
+
+        Feature::create([
+            'image' => 'https://cdn-icons-png.freepik.com/512/929/929430.png',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate Difference',
+            'description' => 'Calculate difference of two numbers',
+            'required_credits' => 3,
+            'active' => true
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20
+        ]);
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100
+        ]);
+        Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credits' => 500
         ]);
     }
 }
