6b218ce88497791e5c43b2712c0b1d6727c72ba3
Joe <youssefbibawy@gmail.com>
2024-08-18 06:36:25 -0500

    remove unused items & edit .editorconfig file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Feature;
-use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,40 +18,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'password' => bcrypt('1234567')
-        ]);
-
-        Feature::create([
-            'image' => 'https://static-00.iconduck/assets.00/plus-icon-2048x2048-z6v59bd6.png',
-            'route_name' => 'feature1.index',
-            'name' => 'Calculate Sum',
-            'description' => 'Calculate sum of two numbers',
-            'required_credits' => 1,
-            'active' => true
-        ]);
-
-        Feature::create([
-            'image' => 'https://cdn-icons-png.freepik.com/512/929/929430.png',
-            'route_name' => 'feature2.index',
-            'name' => 'Calculate Difference',
-            'description' => 'Calculate difference of two numbers',
-            'required_credits' => 3,
-            'active' => true
-        ]);
-
-        Package::create([
-            'name' => 'Basic',
-            'price' => 5,
-            'credits' => 20
-        ]);
-        Package::create([
-            'name' => 'Silver',
-            'price' => 20,
-            'credits' => 100
-        ]);
-        Package::create([
-            'name' => 'Gold',
-            'price' => 50,
-            'credits' => 500
         ]);
     }
 }
